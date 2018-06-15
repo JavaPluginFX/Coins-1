6a4d2cdc2236ac1f8f059e5e77e7e1b547b63ef2
Legendry_xDielo <40304933+LegendryDileo@users.noreply.github.com>
2018-06-15 21:53:49 +0200

    Create API.php

---
+++ src/Legendry/API.php
@@ -0,0 +1,27 @@
+<?php 
+namespace Legendry;
+
+use Legendry\Coins;
+
+use pocketmine\event\Listeners;
+use pocketmine\Player;
+use pocketmine\
+
+class API implements Listeners {
+    
+    public static function addPlayer(string $name){
+        
+    }
+    
+    public static function setCoins(string $player, int $coins){
+        
+    }
+    
+    public static function addCoins(string $player, int $coins){
+        
+    }
+    
+    public static function removeCoins(string $player, int $coins){
+        
+    }    
+}
